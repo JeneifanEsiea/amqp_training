40e8ead338535444f0a59c8d0cc66bd4273c72d3
JeneifanEsiea <chathianathan@et.esiea.fr>
2021-12-08 22:41:58 +0100

    Last 10 messages

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -1,13 +1,16 @@
-
 package fr.lernejo.chat;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChatMessageListener {
+    private final ChatMessageRepository repository;
 
+    public ChatMessageListener(ChatMessageRepository repository) {
+        this.repository = repository;
+    }
 
     public void onMessage(String msg) {
-        System.out.println(msg);
+        repository.addChatMessage(msg);
     }
 }
